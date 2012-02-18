cdf7e6958c626713cd0085e1c825bf936e47e7a3
George Buckenham <v.twenty.one@gmail.com>
2012-02-18 20:48:35 +0000

    server-side commit

---
+++ server/store.php
@@ -0,0 +1,24 @@
+<?php
+$word = $_REQUEST["word"];
+$phrase = $_REQUEST["phrase"];
+
+$words_in_phrase = explode(" ", $phrase);
+for ($i = 0; $i < strlen($word); $i++) {
+    if ( $words_in_phrase[$i][0] != $word[$i]){
+        die("failure__not_cromulent");
+    }
+}
+
+
+$mysqli = new mysqli("localhost", "v21", "", "v21");
+
+if($mysqli->query(sprintf("insert into heftyseamstress( `word`,`phrase`) values (\"%s\", \"%s\");", $mysqli->real_escape_string($word),  $mysqli->real_escape_string($phrase))) === TRUE)
+{
+        die("success");
+}
+else
+{
+        die("failure");
+}
+
+?>
