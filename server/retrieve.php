779652674478e74e85de889d447a7a5ac88fb9c8
George Buckenham <v.twenty.one@gmail.com>
2012-02-19 15:24:34 +0000

    let's declare success

--- server/retrieve.php
+++ server/retrieve.php
@@ -4,7 +4,7 @@ $word = $_REQUEST["word"];
 if ($word == null) {
         die("You ogre! Unhand that hefty seamstress!");
 }
-
+$word = strtolower($word);
 $mysqli = new mysqli("localhost", "v21", "", "v21");
 if($result = $mysqli->query(sprintf("select * from heftyseamstress where word = \"%s\"", $mysqli->real_escape_string($word))))
 {
